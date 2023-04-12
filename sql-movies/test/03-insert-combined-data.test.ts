52e59755af8146409deef2bb6fdcf35d5d43124e
Arnis Pētersons <arnisp03@gmail.com>
2023-04-12 20:41:46 +0300

    03test

--- sql-movies/test/03-insert-combined-data.test.ts
+++ sql-movies/test/03-insert-combined-data.test.ts
@@ -11,7 +11,15 @@ import { Rating } from "../src/data/types";
 import { minutes } from "./utils";
 
 const insertRatings = (movieId: number, ratings: Rating[]) => {
-  throw new Error(`todo`);
+  return (
+    `INSERT INTO ${MOVIE_RATINGS} (user_id, movie_id, rating, time_created) VALUES` +
+    ratings.map(rating => `(
+      '${rating.userId}',
+      '${movieId}',
+      '${rating.rating}',
+      '${rating.time_created}'
+    )`)
+  )
 };
 
 describe("Insert Combined Data", () => {
